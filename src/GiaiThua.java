0dd919f31674ed82f90e5e7e8771483c767535ae
ivantuanvuong <tuamphann5@gmail.com>
2024-09-13 22:38:33 +0700

    update

--- src/GiaiThua.java
+++ src/GiaiThua.java
@@ -1,10 +1,10 @@
 public class GiaiThua {
 
     public int GiaiThua(int nums) {
-        if (nums > 0) {
-            return nums * GiaiThua(nums - 1);
-        } else {
+        if (nums < 0) {
             return 1;
+        } else {
+            return nums * GiaiThua(nums - 1);
         }
     }
 }
